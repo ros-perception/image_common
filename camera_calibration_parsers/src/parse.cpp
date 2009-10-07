6fcf01106db0e2ca5d41e3b1b031e3c8afd7afbf
mihelich <mihelich@eb33c2ac-9c88-4c90-87e0-44a10359b0c3>
2009-10-07 02:55:04 +0000

    camera_calibration_parsers: Removed deprecated functions.

--- camera_calibration_parsers/src/parse.cpp
+++ camera_calibration_parsers/src/parse.cpp
@@ -9,65 +9,32 @@ namespace camera_calibration_parsers {
 bool writeCalibration(const std::string& file_name, const std::string& camera_name,
                       const sensor_msgs::CameraInfo& cam_info)
 {
-  return writeCalibration(file_name, camera_name, cam_info.width, cam_info.height,
-                          &cam_info.K[0], &cam_info.D[0], &cam_info.R[0], &cam_info.P[0]);
+  if (boost::iends_with(file_name, ".ini"))
+    return writeCalibrationIni(file_name, camera_name, cam_info);
+  if (boost::iends_with(file_name, ".yml") || boost::iends_with(file_name, ".yaml"))
+    return writeCalibrationYml(file_name, camera_name, cam_info);
+
+  return false;
 }
 
 bool readCalibration(const std::string& file_name, std::string& camera_name,
                      sensor_msgs::CameraInfo& cam_info)
 {
-  int width, height;
-  bool success = readCalibration(file_name, camera_name, width, height,
-                                 &cam_info.K[0], &cam_info.D[0], &cam_info.R[0], &cam_info.P[0]);
-  cam_info.width = width;
-  cam_info.height = height;
-  return success;
+  if (boost::iends_with(file_name, ".ini"))
+    return readCalibrationIni(file_name, camera_name, cam_info);
+  if (boost::iends_with(file_name, ".yml") || boost::iends_with(file_name, ".yaml"))
+    return readCalibrationYml(file_name, camera_name, cam_info);
+
+  return false;
 }
 
 bool parseCalibration(const std::string& buffer, const std::string& format,
                       std::string& camera_name, sensor_msgs::CameraInfo& cam_info)
 {
-  int width, height;
-  bool success = parseCalibration(buffer, format, camera_name, width, height,
-                                  &cam_info.K[0], &cam_info.D[0], &cam_info.R[0], &cam_info.P[0]);
-  cam_info.width = width;
-  cam_info.height = height;
-  return success;
-}
-
-bool writeCalibration(const std::string& file_name, const std::string& camera_name,
-                     int width, int height,
-                     const double* K, const double* D,
-                     const double* R, const double* P)
-{
-  if (boost::iends_with(file_name, ".ini"))
-    return writeCalibrationIni(file_name, camera_name, width, height, K, D, R, P);
-  if (boost::iends_with(file_name, ".yml") || boost::iends_with(file_name, ".yaml"))
-    return writeCalibrationYml(file_name, camera_name, width, height, K, D, R, P);
-
-  return false;
-}
-
-bool readCalibration(const std::string& file_name, std::string& camera_name,
-                    int &width, int &height,
-                    double* K, double* D, double* R, double* P)
-{
-  if (boost::iends_with(file_name, ".ini"))
-    return readCalibrationIni(file_name, camera_name, width, height, K, D, R, P);
-  if (boost::iends_with(file_name, ".yml") || boost::iends_with(file_name, ".yaml"))
-    return readCalibrationYml(file_name, camera_name, width, height, K, D, R, P);
-
-  return false;
-}
-
-bool parseCalibration(const std::string& buffer, const std::string& format,
-                      std::string& camera_name, int &width, int &height,
-                      double* K, double* D, double* R, double* P)
-{
   if (format != "ini")
     return false;
 
-  return parseCalibrationIni(buffer, camera_name, width, height, K, D, R, P);
+  return parseCalibrationIni(buffer, camera_name, cam_info);
 }
 
 } //namespace camera_calibration_parsers
--- camera_calibration_parsers/src/parse_ini.cpp
+++ camera_calibration_parsers/src/parse_ini.cpp
@@ -28,74 +28,29 @@ static void printMatrix(FILE* file, const double* M, int rows, int cols, const c
 bool writeCalibrationIni(const std::string& file_name, const std::string& camera_name,
                          const sensor_msgs::CameraInfo& cam_info)
 {
-  return writeCalibrationIni(file_name, camera_name, cam_info.width, cam_info.height,
-                             &cam_info.K[0], &cam_info.D[0], &cam_info.R[0], &cam_info.P[0]);
-}
-
-bool readCalibrationIni(const std::string& file_name, std::string& camera_name,
-                        sensor_msgs::CameraInfo& cam_info)
-{
-  int width, height;
-  bool success = readCalibrationIni(file_name, camera_name, width, height,
-                                    &cam_info.K[0], &cam_info.D[0], &cam_info.R[0], &cam_info.P[0]);
-  cam_info.width = width;
-  cam_info.height = height;
-  return success;
-}
-
-bool parseCalibrationIni(const std::string& buffer, std::string& camera_name,
-                         sensor_msgs::CameraInfo& cam_info)
-{
-  int width, height;
-  bool success = parseCalibrationIni(buffer, camera_name, width, height,
-                                     &cam_info.K[0], &cam_info.D[0], &cam_info.R[0], &cam_info.P[0]);
-  cam_info.width = width;
-  cam_info.height = height;
-  return success;
-}
-
-bool writeCalibrationIni(const std::string& file_name, const std::string& camera_name,
-                         int width, int height,
-                         const double* K, const double* D,
-                         const double* R, const double* P)
-{
   FILE* file = fopen(file_name.c_str(), "w");
   if (!file)
     return false;
   boost::shared_ptr<FILE> guard(file, fclose);
 
   fprintf(file, "# Camera intrinsics\n\n");
-  /** @todo: time? */
+  /// @todo time?
   fprintf(file, "[image]\n\n");
-  fprintf(file, "width\n%d\n\n", width);
-  fprintf(file, "height\n%d\n\n", height);
-
+  fprintf(file, "width\n%d\n\n", cam_info.width);
+  fprintf(file, "height\n%d\n\n", cam_info.height);
   fprintf(file, "[%s]\n\n", camera_name.c_str());
+
   fprintf(file, "camera matrix\n");
-  printMatrix(file, K, 3, 3);
-  
+  printMatrix(file, &cam_info.K[0], 3, 3);
+
   fprintf(file, "\ndistortion\n");
-  if (D) {
-    printMatrix(file, D, 1, 5);
-  } else {
-    fprintf(file, "0.00000 0.00000 0.00000 0.00000 0.00000\n");
-  }
+  printMatrix(file, &cam_info.D[0], 1, 5);
   
   fprintf(file, "\n\nrectification\n");
-  if (R) {
-    printMatrix(file, R, 3, 3);
-  } else {
-    fprintf(file, "1.00000 0.00000 0.00000\n");
-    fprintf(file, "0.00000 1.00000 0.00000\n");
-    fprintf(file, "0.00000 0.00000 1.00000\n");
-  }
+  printMatrix(file, &cam_info.R[0], 3, 3);
 
   fprintf(file, "\nprojection\n");
-  if (P) {
-    printMatrix(file, P, 3, 4);
-  } else {
-    printMatrix(file, K, 3, 3, "0.00000\n");
-  }
+  printMatrix(file, &cam_info.P[0], 3, 4);
 
   return true;
 }
@@ -126,27 +81,20 @@ ArrayAssignActor<T> array_assign_a(T* start)
 
 template <typename Iterator>
 bool parseCalibrationIniRange(Iterator first, Iterator last,
-                              std::string& camera_name, int& width, int& height,
-                              double* K, double* D, double* R, double* P)
+                              std::string& camera_name, sensor_msgs::CameraInfo& cam_info)
 {
-  /** @todo: actually parse only what's requested? */
-  double ignore[12];
-  if (!D) D = ignore;
-  if (!R) R = ignore;
-  if (!P) P = ignore;
-  
   bool have_externals = false;
   double trans[3], rot[3];
 
-  /** @todo: separate grammar out into separate function */
+  /// @todo separate grammar out into separate function
   
   // Image section (width, height)
   BOOST_AUTO(image,
       str_p("[image]")
       >> "width"
-      >> uint_p[assign_a(width)]
+      >> uint_p[assign_a(cam_info.width)]
       >> "height"
-      >> uint_p[assign_a(height)]
+      >> uint_p[assign_a(cam_info.height)]
      );
 
   // Optional externals section
@@ -165,13 +113,13 @@ bool parseCalibrationIniRange(Iterator first, Iterator last,
   BOOST_AUTO(camera,
       name
       >> "camera matrix"
-      >> repeat_p(9)[real_p[array_assign_a(K)]]
+      >> repeat_p(9)[real_p[array_assign_a(&cam_info.K[0])]]
       >> "distortion"
-      >> repeat_p(5)[real_p[array_assign_a(D)]]
+      >> repeat_p(5)[real_p[array_assign_a(&cam_info.D[0])]]
       >> "rectification"
-      >> repeat_p(9)[real_p[array_assign_a(R)]]
+      >> repeat_p(9)[real_p[array_assign_a(&cam_info.R[0])]]
       >> "projection"
-      >> repeat_p(12)[real_p[array_assign_a(P)]]
+      >> repeat_p(12)[real_p[array_assign_a(&cam_info.P[0])]]
      );
 
   // Full grammar
@@ -184,29 +132,26 @@ bool parseCalibrationIniRange(Iterator first, Iterator last,
   BOOST_AUTO(skip, space_p | comment_p('#'));
 
   parse_info<Iterator> info = parse(first, last, ini_grammar, skip);
-  /** @todo: do anything with externals? */
+  /// @todo Do something with externals?
   return info.hit;
 }
 /// \endcond
 
 bool readCalibrationIni(const std::string& file_name, std::string& camera_name,
-                        int &width, int &height,
-                        double* K, double* D, double* R, double* P)
+                        sensor_msgs::CameraInfo& cam_info)
 {
   typedef file_iterator<char> Iterator;
 
   Iterator first(file_name);
   Iterator last = first.make_end();
 
-  return parseCalibrationIniRange(first, last, camera_name, width, height, K, D, R, P);
+  return parseCalibrationIniRange(first, last, camera_name, cam_info);
 }
 
 bool parseCalibrationIni(const std::string& buffer, std::string& camera_name,
-                        int &width, int &height,
-                        double* K, double* D, double* R, double* P)
+                         sensor_msgs::CameraInfo& cam_info)
 {
-  return parseCalibrationIniRange(buffer.begin(), buffer.end(), camera_name,
-                                  width, height, K, D, R, P);
+  return parseCalibrationIniRange(buffer.begin(), buffer.end(), camera_name, cam_info);
 }
 
 } //namespace camera_calibration_parsers
