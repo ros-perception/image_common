ab6a0a9710b7941ece86d79a7ceea6fe0d95f5f1
Vincent Rabaud <vrabaud@willowgarage.com>
2013-01-03 16:10:44 +0100

    use the pluginlib script to remove some warnings

--- image_transport/src/manifest.cpp
+++ image_transport/src/manifest.cpp
@@ -36,6 +36,6 @@
 #include "image_transport/raw_publisher.h"
 #include "image_transport/raw_subscriber.h"
 
-PLUGINLIB_DECLARE_CLASS(image_transport, raw_pub, image_transport::RawPublisher, image_transport::PublisherPlugin)
+PLUGINLIB_EXPORT_CLASS( image_transport::RawPublisher, image_transport::PublisherPlugin)
 
-PLUGINLIB_DECLARE_CLASS(image_transport, raw_sub, image_transport::RawSubscriber, image_transport::SubscriberPlugin)
+PLUGINLIB_EXPORT_CLASS( image_transport::RawSubscriber, image_transport::SubscriberPlugin)
