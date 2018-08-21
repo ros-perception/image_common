cef620da2c4a4f62ea7705220e8d64eedd1510f2
Michael Carroll <michael@openrobotics.org>
2018-08-21 11:46:58 -0500

    Apply ROS2 style to polled_camera.

--- polled_camera/src/publication_server.cpp
+++ polled_camera/src/publication_server.cpp
@@ -1,13 +1,13 @@
 /*********************************************************************
 * Software License Agreement (BSD License)
-* 
+*
 *  Copyright (c) 2009, Willow Garage, Inc.
 *  All rights reserved.
-* 
+*
 *  Redistribution and use in source and binary forms, with or without
 *  modification, are permitted provided that the following conditions
 *  are met:
-* 
+*
 *   * Redistributions of source code must retain the above copyright
 *     notice, this list of conditions and the following disclaimer.
 *   * Redistributions in binary form must reproduce the above
@@ -17,7 +17,7 @@
 *   * Neither the name of the Willow Garage nor the names of its
 *     contributors may be used to endorse or promote products derived
 *     from this software without specific prior written permission.
-* 
+*
 *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -35,7 +35,8 @@
 #include "polled_camera/publication_server.h"
 #include <image_transport/image_transport.h>
 
-namespace polled_camera {
+namespace polled_camera
+{
 
 /// \cond
 
@@ -49,18 +50,20 @@ public:
   std::map<std::string, image_transport::CameraPublisher> client_map_;
   bool unadvertised_;
   double constructed_;
-  
-  Impl(const ros::NodeHandle& nh)
-    : it_(nh),
-      unadvertised_(false),
-      constructed_(ros::WallTime::now().toSec())
+
+  Impl(const ros::NodeHandle & nh)
+  : it_(nh),
+    unadvertised_(false),
+    constructed_(ros::WallTime::now().toSec())
   {
   }
-  
+
   ~Impl()
   {
-    if (ros::WallTime::now().toSec() - constructed_ < 0.001)
-      ROS_WARN("PublicationServer destroyed immediately after creation. Did you forget to store the handle?");
+    if (ros::WallTime::now().toSec() - constructed_ < 0.001) {
+      ROS_WARN(
+        "PublicationServer destroyed immediately after creation. Did you forget to store the handle?");
+    }
     unadvertise();
   }
 
@@ -78,17 +81,18 @@ public:
     }
   }
 
-  bool requestCallback(polled_camera::GetPolledImage::Request& req,
-                       polled_camera::GetPolledImage::Response& rsp)
+  bool requestCallback(
+    polled_camera::GetPolledImage::Request & req,
+    polled_camera::GetPolledImage::Response & rsp)
   {
     std::string image_topic = req.response_namespace + "/image_raw";
-    image_transport::CameraPublisher& pub = client_map_[image_topic];
+    image_transport::CameraPublisher & pub = client_map_[image_topic];
     if (!pub) {
       // Create a latching camera publisher.
       pub = it_.advertiseCamera(image_topic, 1, image_transport::SubscriberStatusCallback(),
-                                boost::bind(&Impl::disconnectCallback, this, _1),
-                                ros::SubscriberStatusCallback(), ros::SubscriberStatusCallback(),
-                                ros::VoidPtr(), true /*latch*/);
+          boost::bind(&Impl::disconnectCallback, this, _1),
+          ros::SubscriberStatusCallback(), ros::SubscriberStatusCallback(),
+          ros::VoidPtr(), true /*latch*/);
       ROS_INFO("Advertising %s", pub.getTopic().c_str());
     }
 
@@ -100,21 +104,20 @@ public:
     sensor_msgs::Image image;
     sensor_msgs::CameraInfo info;
     driver_cb_(req, rsp, image, info);
-    
+
     if (rsp.success) {
       assert(image.header.stamp == info.header.stamp);
       rsp.stamp = image.header.stamp;
       pub.publish(image, info);
-    }
-    else {
+    } else {
       ROS_ERROR("Failed to capture requested image, status message: '%s'",
-                rsp.status_message.c_str());
+        rsp.status_message.c_str());
     }
-    
+
     return true; // Success/failure indicated by rsp.success
   }
 
-  void disconnectCallback(const image_transport::SingleSubscriberPublisher& ssp)
+  void disconnectCallback(const image_transport::SingleSubscriberPublisher & ssp)
   {
     // Shut down the publication when the subscription count drops to zero.
     if (ssp.getNumSubscribers() == 0) {
@@ -126,9 +129,10 @@ public:
 
 /// \endcond
 
-PublicationServer::PublicationServer(const std::string& service, ros::NodeHandle& nh,
-                                     const DriverCallback& cb, const ros::VoidPtr& tracked_object)
-  : impl_(new Impl(nh))
+PublicationServer::PublicationServer(
+  const std::string & service, ros::NodeHandle & nh,
+  const DriverCallback & cb, const ros::VoidPtr & tracked_object)
+: impl_(new Impl(nh))
 {
   impl_->driver_cb_ = cb;
   impl_->tracked_object_ = tracked_object;
@@ -137,23 +141,24 @@ PublicationServer::PublicationServer(const std::string& service, ros::NodeHandle
 
 void PublicationServer::shutdown()
 {
-  if (impl_) impl_->unadvertise();
+  if (impl_) {impl_->unadvertise();}
 }
 
 std::string PublicationServer::getService() const
 {
-  if (impl_) return impl_->srv_server_.getService();
+  if (impl_) {return impl_->srv_server_.getService();}
   return std::string();
 }
 
-PublicationServer::operator void*() const
+PublicationServer::operator void *() const
 {
-  return (impl_ && impl_->isValid()) ? (void*)1 : (void*)0;
+  return (impl_ && impl_->isValid()) ? (void *)1 : (void *)0;
 }
 
-PublicationServer advertise(ros::NodeHandle& nh, const std::string& service,
-                            const PublicationServer::DriverCallback& cb,
-                            const ros::VoidPtr& tracked_object)
+PublicationServer advertise(
+  ros::NodeHandle & nh, const std::string & service,
+  const PublicationServer::DriverCallback & cb,
+  const ros::VoidPtr & tracked_object)
 {
   return PublicationServer(service, nh, cb, tracked_object);
 }
