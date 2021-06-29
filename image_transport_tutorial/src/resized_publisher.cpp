f81dcd9eea4d6b769af7104a0e187d2d6e015b75
Rebecca Butler <rebecca@openrobotics.org>
2021-06-29 12:16:22 -0400

    Lint
    Signed-off-by: Rebecca Butler <rebecca@openrobotics.org>

--- image_transport_tutorial/src/resized_publisher.cpp
+++ image_transport_tutorial/src/resized_publisher.cpp
@@ -1,22 +1,38 @@
-#include <opencv2/imgproc/imgproc.hpp>
+// Copyright 2021, Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <memory>
 
 #include "image_transport_tutorial/resized_publisher.hpp"
 #include "rclcpp/logging.hpp"
 
-void ResizedPublisher::publish(const sensor_msgs::msg::Image& message,
-                               const PublishFn& publish_fn) const
+void ResizedPublisher::publish(
+  const sensor_msgs::msg::Image & message,
+  const PublishFn & publish_fn) const
 {
   cv::Mat cv_image;
   std::shared_ptr<void const> tracked_object;
-  try
-  {
+  try {
     cv_image = cv_bridge::toCvShare(message, tracked_object, message.encoding)->image;
-  }
-  catch (cv::Exception &e)
-  {
+  } catch (cv::Exception & e) {
     auto logger = rclcpp::get_logger("resized_publisher");
-    RCLCPP_ERROR(logger, "Could not convert from '%s' to '%s'.", message.encoding.c_str(), message.encoding.c_str());
+    RCLCPP_ERROR(
+      logger, "Could not convert from '%s' to '%s'.",
+      message.encoding.c_str(), message.encoding.c_str());
     return;
   }
 
