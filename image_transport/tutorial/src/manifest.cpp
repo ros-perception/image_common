ede3526ccf798a5db1049f20646241cdf0e0926e
Rebecca Butler <rebecca@openrobotics.org>
2021-08-13 10:31:14 -0400

    Delete tutorial folder
    Signed-off-by: Rebecca Butler <rebecca@openrobotics.org>

--- image_transport/tutorial/src/manifest.cpp
+++
@@ -1,7 +0,0 @@
-#include <pluginlib/class_list_macros.h>
-#include <image_transport_tutorial/resized_publisher.h>
-#include <image_transport_tutorial/resized_subscriber.h>
-
-PLUGINLIB_EXPORT_CLASS(ResizedPublisher, image_transport::PublisherPlugin)
-
-PLUGINLIB_EXPORT_CLASS(ResizedSubscriber, image_transport::SubscriberPlugin)
