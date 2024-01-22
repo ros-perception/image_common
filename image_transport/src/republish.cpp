7049db52dc795131de70feb7c28a07559d1a33be
Carlos Andrés Álvarez Restrepo <charlie_cha@outlook.com>
2024-01-22 13:27:22 -0500

    Add QoS option reliability to republisher qos params (#296)

--- image_transport/src/republish.cpp
+++ image_transport/src/republish.cpp
@@ -67,6 +67,7 @@ int main(int argc, char ** argv)
       rclcpp::QosPolicyKind::Depth,
       rclcpp::QosPolicyKind::Durability,
       rclcpp::QosPolicyKind::History,
+      rclcpp::QosPolicyKind::Reliability,
     });
 
     pub_options.qos_overriding_options = qos_override_options;
