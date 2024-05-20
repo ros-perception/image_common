07b7bc7630b8e5b6e0e763f2b72b3d19af39a772
Benjamin Tan <tan.benjaminkx@gmail.com>
2024-05-20 21:50:51 +0800

    Update copyright to 2024

--- image_transport/test/test_message_passing_lifecycle.cpp
+++ image_transport/test/test_message_passing_lifecycle.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 Open Source Robotics Foundation, Inc.
+// Copyright (c) 2024 Open Source Robotics Foundation, Inc.
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
