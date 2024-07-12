acc5f39e535674c23a2848eb3d579444de0b3bee
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-07-12 14:15:04 +0200

    Added common test to camera info manager
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- camera_info_manager/include/camera_info_manager/visibility_control.h
+++ camera_info_manager/include/camera_info_manager/visibility_control.h
@@ -1,4 +1,5 @@
 // Copyright 2020 Microsoft Corporation.
+// All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
