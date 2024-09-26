4ceb2ecbd33d64b3c8081e3baf5a2d1ea1598cfe
Tamas Foldi <tfoldi@xsi.hu>
2024-09-26 07:50:47 +0200

    WIP: before moving spin to custom class

--- image_transport_py/src/image_transport_py/pybind_image_transport.cpp
+++ image_transport_py/src/image_transport_py/pybind_image_transport.cpp
@@ -27,9 +27,30 @@
 #include <rclcpp/serialization.hpp>
 #include <sensor_msgs/msg/image.hpp>
 
+using namespace std::chrono_literals;
+
 namespace image_transport_python
 {
-// Bindings for the ImageCodec class
+
+class PyImageTransport : public image_transport::ImageTransport {
+public:
+    // Constructor that takes an rclcpp::Node::SharedPtr
+    PyImageTransport(const rclcpp::Node::SharedPtr& node)
+        : image_transport::ImageTransport(node) {}
+
+    // Public member variable for the subscriber
+    thread_local image_transport::Subscriber subscriber_;
+};
+
+
+void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr & msg)
+{
+  RCLCPP_INFO(
+    rclcpp::get_logger(
+      "image_transport"), "message arrived yoho. %s", msg->encoding.c_str());
+
+}
+// Bindings for the image_transport classes
 PYBIND11_MODULE(_image_transport, m)
 {
   m.doc() = "Python wrapper of the image_transport API";
@@ -50,7 +71,27 @@ PYBIND11_MODULE(_image_transport, m)
     },
     "Publish an image on the topics associated with this Publisher.");
 
-  pybind11::class_<image_transport::ImageTransport>(m, "ImageTransport")
+  pybind11::class_<image_transport::CameraPublisher>(m, "CameraPublisher")
+  .def(pybind11::init())
+  .def(
+    "get_topic", &image_transport::CameraPublisher::getTopic,
+    "Returns the base (image) topic of this CameraPublisher.")
+  .def(
+    "get_num_subscribers", &image_transport::CameraPublisher::getNumSubscribers,
+    "Returns the number of subscribers this camera publisher is connected to.")
+  .def(
+    "shutdown", &image_transport::CameraPublisher::shutdown,
+    "Unsubscribe the callback associated with this CameraPublisher.")
+  .def(
+    "publish",
+    [](image_transport::CameraPublisher & publisher, sensor_msgs::msg::Image & img,
+    sensor_msgs::msg::CameraInfo & info) {
+      publisher.publish(img, info);
+    },
+    "Publish an image and camera info on the topics associated with this Publisher.");
+
+  // pybind11::class_<image_transport::ImageTransport>(m, "ImageTransport")
+  pybind11::class_<image_transport_python::PyImageTransport>(m, "ImageTransport")
   .def(
     pybind11::init(
       [](const std::string & node_name, const std::string & launch_params_filepath) {
@@ -66,22 +107,84 @@ PYBIND11_MODULE(_image_transport, m)
         }
         rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared(node_name, "", node_options);
 
-        std::shared_ptr<rclcpp::executors::SingleThreadedExecutor> executor =
-        std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
+
+        RCLCPP_INFO(node->get_logger(), "starting thread");
+        std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> executor =
+        std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
 
         auto spin_node = [node, executor]() {
+        // auto subscription = node->create_subscription<sensor_msgs::msg::Image>(
+        //   "camera/image", 10, image_transport_python::imageCallback);
+
+        RCLCPP_INFO(node->get_logger(), "spin on!");
         executor->add_node(node);
         executor->spin();
+        // while (rclcpp::ok()) {
+        // Spin some to process available callbacks, but do not block indefinitely
+        // executor->spin_some();  // Spin for up to 100ms at a time
+        // std::this_thread::sleep_for(1000ms);  // Simulate other tasks or waiting
+        // RCLCPP_INFO(node->get_logger(), "spinnn!");
+        // }
+
+        RCLCPP_INFO(node->get_logger(), "spin off!");
       };
         std::thread execution_thread(spin_node);
         execution_thread.detach();
 
-        return image_transport::ImageTransport(node);
+        // return image_transport::ImageTransport(node);
+        return image_transport_python::PyImageTransport(node);
       }))
   .def(
     "advertise",
     pybind11::overload_cast<const std::string &, uint32_t, bool>(
       &image_transport::ImageTransport::advertise),
-    pybind11::arg("base_topic"), pybind11::arg("queue_size"), pybind11::arg("latch") = false);
+    pybind11::arg("base_topic"), pybind11::arg("queue_size"), pybind11::arg("latch") = false)
+  .def(
+    "advertise_camera",
+    pybind11::overload_cast<const std::string &, uint32_t, bool>(
+      &image_transport::ImageTransport::advertiseCamera),
+    pybind11::arg("base_topic"), pybind11::arg("queue_size"), pybind11::arg("latch") = false)
+  .def(
+    "subscribe",
+    [](image_transport_python::PyImageTransport & image_transport,
+    const std::string & base_topic,
+    uint32_t queue_size,
+    const std::function<void(const sensor_msgs::msg::Image::ConstSharedPtr &)> & callback) {
+
+      RCLCPP_INFO(image_transport.impl_->node_->get_logger(), "in subscribe");
+
+      // image_transport.subscriber_ = std::make_unique<image_transport::Subscriber>(image_transport.subscribe(base_topic, queue_size, image_transport_python::imageCallback)) ;
+      // auto subscription = 
+      thread_local auto  subscription = std::make_shared<image_transport::Subscriber>(image_transport.subscribe(base_topic, queue_size, image_transport_python::imageCallback)) ;
+      // RCLCPP_INFO(image_transport.impl_->node_->get_logger(), "subscribed to %s", subscription.getTopic().c_str());
+
+      return subscription;
+
+  },  
+    pybind11::arg("base_topic"), pybind11::arg("queue_size"), pybind11::arg("callback"))
+
+  ;
+
+  pybind11::class_<image_transport::Subscriber, std::shared_ptr<image_transport::Subscriber>>(
+    m,
+    "Subscriber")
+  .def(pybind11::init())
+  .def(
+    "get_topic",
+    &image_transport::Subscriber::getTopic,
+    "Returns the base image topic.")
+  .def(
+    "get_num_publishers",
+    &image_transport::Subscriber::getNumPublishers,
+    "Returns the number of publishers this subscriber is connected to.")
+  .def(
+    "get_transport",
+    &image_transport::Subscriber::getTransport,
+    "Returns the name of the transport being used.")
+  .def(
+    "shutdown", &image_transport::Subscriber::shutdown,
+    "Unsubscribe the callback associated with this Subscriber.");
+
+
 }
 }  // namespace image_transport_python
