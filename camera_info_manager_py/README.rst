b09118a0ec67982151b02a85fe76997820fea8fa
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-11-25 18:34:27 +0100

    Add `camera_info_manager_py` (#335)
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    (cherry picked from commit d09c82cfb5558d253de99317a4d7d5fb61867b03)

---
+++ camera_info_manager_py/README.rst
@@ -0,0 +1,16 @@
+Overview
+========
+
+This ROS_ package defines a Python camera_info_manager interface,
+providing `sensor_msgs/CameraInfo`_ support for camera drivers written
+in Python.
+
+ROS wiki documentation: `camera_info_manager_py`_.
+
+This interface is similar to that of the C++ `camera_info_manager`_
+package, but not identical.
+
+.. _ROS: http://ros.org
+.. _`sensor_msgs/CameraInfo`: http://ros.org/doc/api/sensor_msgs/html/msg/CameraInfo.html
+.. _`camera_info_manager`: http://ros.org/wiki/camera_info_manager
+.. _`camera_info_manager_py`: http://ros.org/wiki/camera_info_manager_py
---
+++ camera_info_manager_py/setup.py
@@ -0,0 +1,29 @@
+import os
+
+from setuptools import setup
+
+package_name = 'camera_info_manager_py'
+
+setup(
+    name=package_name,
+    version='6.0.1',
+    packages=['camera_info_manager'],
+    data_files=[
+        # Install marker file in the package index
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        # Include our package.xml file
+        (os.path.join('share', package_name), ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jose Mastrangelo',
+    maintainer_email='jmastrangelo@clearpathrobotics.com',
+    author="Jack O'Quin",
+    author_email='jack.oquin@gmail.com',
+    description='Python interface for camera calibration information.',
+    long_description='Python interface for camera calibration information. \n\n'
+                     'This ROS package provides a CameraInfo interface for Python camera\n'
+                     'drivers similar to the C++ camera_info_manager package.',
+    license='BSD',
+    tests_require=['pytest'],
+)
