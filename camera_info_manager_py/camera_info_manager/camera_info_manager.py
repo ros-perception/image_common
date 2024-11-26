c953603d82bcba1272def4aae53f99e9981676b5
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-11-26 11:57:48 +0100

    fixed copyright
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- camera_info_manager_py/camera_info_manager/camera_info_manager.py
+++ camera_info_manager_py/camera_info_manager/camera_info_manager.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # Copyright 2012, Jack O'Quin
-# All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
