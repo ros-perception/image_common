37155a39da6610ec55fa3ceb8dc4e4274f59b643
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-06-12 13:53:22 +0200

    6.1.2

--- camera_info_manager_py/setup.py
+++ camera_info_manager_py/setup.py
@@ -5,7 +5,7 @@ package_name = 'camera_info_manager_py'
 
 setup(
     name=package_name,
-    version='6.1.1',
+    version='6.1.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         # Install marker file in the package index
