45228b3b42dcc9c063054a9b530eb12093f69a43
mosFet <realeandrea@yahoo.it>
2025-07-02 14:15:28 +0200

    fix python deprecation
    FIX:
    
    #UserWarning: Unknown distribution option: 'tests_require'
    
    and
    
    #SetuptoolsDeprecationWarning: License classifiers are deprecated. !!
    
    ********************************************************************************
    Please consider removing the following classifiers in favor of a SPDX license expression:
    
    License :: OSI Approved :: BSD License
    
    See https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license for details.
    ********************************************************************************

--- camera_info_manager_py/setup.py
+++ camera_info_manager_py/setup.py
@@ -22,7 +22,6 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
@@ -31,5 +30,9 @@ setup(
                      'This ROS package provides a CameraInfo interface for Python camera\n'
                      'drivers similar to the C++ camera_info_manager package.',
     license='BSD',
-    tests_require=['pytest'],
+    extras_require={
+        'test' : [
+            'pytest'
+        ]
+    },
 )
