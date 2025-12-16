c16c26e4209bcb9d0320c873d2a77e5c379746f9
Tomoya Fujita <Tomoya.Fujita@sony.com>
2025-12-16 22:17:18 +0900

    properly shut down rclcpp after all tests complete. (#384)
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>
    (cherry picked from commit 810c5b6f85f4237e6be6e16500af083423e08061)
    
    # Conflicts:
    #	image_transport/include/image_transport/camera_common.h

--- image_transport/include/image_transport/camera_common.h
+++ image_transport/include/image_transport/camera_common.h
@@ -33,4 +33,63 @@
 
 #include "camera_common.hpp"
 
+<<<<<<< HEAD:image_transport/include/image_transport/camera_common.h
 #endif  // IMAGE_TRANSPORT__CAMERA_COMMON_H_
+=======
+#include "image_transport/single_subscriber_publisher.hpp"
+
+#include "utils.hpp"
+
+class TestPublisherLifecycle : public ::testing::Test
+{
+protected:
+  static constexpr const char * caller_id = "node";
+  static constexpr const char * topic = "/topic";
+
+
+  static void SetUpTestCase()
+  {
+    rclcpp::init(0, nullptr);
+  }
+
+  void SetUp()
+  {
+    node = std::make_shared<rclcpp_lifecycle::LifecycleNode>("image_transport", "/ns");
+  }
+
+  void TearDown()
+  {
+    node.reset();
+  }
+
+  static void TearDownTestSuite()
+  {
+    rclcpp::shutdown();
+  }
+
+  rclcpp_lifecycle::LifecycleNode::SharedPtr node;
+};
+
+TEST_F(TestPublisherLifecycle, construction_and_destruction) {
+  auto get_num_subscribers = []() -> size_t {return 0;};
+  auto publish_fn = [](const sensor_msgs::msg::Image & /*image*/) {};
+
+  image_transport::SingleSubscriberPublisher ssp(caller_id, topic,
+    get_num_subscribers, publish_fn);
+}
+
+TEST_F(TestPublisherLifecycle, getNumSubscribers) {
+  size_t nSub = 0;
+
+  auto get_num_subscribers = [&nSub]() -> size_t {return nSub;};
+  auto publish_fn = [](const sensor_msgs::msg::Image & /*image*/) {};
+
+  image_transport::SingleSubscriberPublisher ssp(caller_id, topic,
+    get_num_subscribers, publish_fn);
+
+  nSub = 0;
+  ASSERT_EQ(ssp.getNumSubscribers(), 0u);
+  nSub = 1;
+  ASSERT_EQ(ssp.getNumSubscribers(), 1u);
+}
+>>>>>>> 810c5b6 (properly shut down rclcpp after all tests complete. (#384)):image_transport/test/test_single_subscriber_publisher_lifecycle.cpp
