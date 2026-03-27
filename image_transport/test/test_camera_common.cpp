f82f30f5365685ad13a9b305f94ff319f115ffee
Martin Pecka <peci1@seznam.cz>
2026-03-27 09:28:04 +0100

    Improvements to #394 (#398)
    Signed-off-by: Martin Pecka <peckama2@fel.cvut.cz>

--- image_transport/test/test_camera_common.cpp
+++ image_transport/test/test_camera_common.cpp
@@ -28,6 +28,8 @@
 
 #include <gtest/gtest.h>
 
+#include <typeinfo>
+
 #include "image_transport/camera_common.hpp"
 
 TEST(CameraCommon, getCameraInfoTopic_namespaced_topic) {
@@ -110,6 +112,27 @@ TEST(CameraCommon, erase_last_copy) {
   EXPECT_EQ("/image/image", image_transport::erase_last_copy("/image_pub/image", "_pub"));
 }
 
+struct A
+{
+  virtual ~A() = default;
+};
+
+struct B : A {};
+
+TEST(CameraCommon, demangle) {
+  using image_transport::demangle_cpp_type_name;
+  using image_transport::PluginManifestData;
+
+  EXPECT_EQ("int", demangle_cpp_type_name(typeid(int).name()));  // NOLINT(readability/casting)
+  EXPECT_EQ("image_transport::PluginManifestData",
+    demangle_cpp_type_name(typeid(PluginManifestData{}).name()));
+  EXPECT_EQ("A", demangle_cpp_type_name(typeid(A{}).name()));
+  EXPECT_EQ("B", demangle_cpp_type_name(typeid(B{}).name()));
+  A * a = new B;
+  EXPECT_EQ("B", demangle_cpp_type_name(typeid(*a).name()));
+  delete a;
+}
+
 // ---------------------------------------------------------------------------
 // Tests that verify per-class overrides take precedence over library defaults.
 // TEST_PLUGIN_MANIFEST_XML is injected as a compile definition from CMakeLists.txt.
@@ -141,4 +164,23 @@ TEST(PluginManifestXml, library_level_message_type_used_as_fallback) {
     "sensor_msgs/msg/Image",
     image_transport::get_message_type_from_manifest(
       TEST_PLUGIN_MANIFEST_XML, "image_transport/lib_fallback_pub"));
+}
+
+// ---------------------------------------------------------------------------
+// Tests that verify fallback to lookup name parsing.
+// TEST_PLUGIN_MANIFEST_XML is injected as a compile definition from CMakeLists.txt.
+// ---------------------------------------------------------------------------
+
+TEST(PluginManifestXml, fallback_to_lookup_name_pub) {
+  EXPECT_EQ(
+    "lookup",
+    image_transport::get_transport_name_from_manifest(
+      TEST_PLUGIN_MANIFEST_XML, "image_transport/lookup_pub"));
+}
+
+TEST(PluginManifestXml, fallback_to_lookup_name_sub) {
+  EXPECT_EQ(
+    "lookup",
+    image_transport::get_transport_name_from_manifest(
+      TEST_PLUGIN_MANIFEST_XML, "image_transport/lookup_sub"));
 }
