69b50c6db906b2044502b00c1b35dc97ec8a40ce
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-05-29 10:42:48 +0200

    3.1.13

--- camera_info_manager_py/setup.py
+++ camera_info_manager_py/setup.py
@@ -6,7 +6,7 @@ package_name = 'camera_info_manager_py'
 
 setup(
     name=package_name,
-    version='3.1.12',
+    version='3.1.13',
     packages=['camera_info_manager'],
     data_files=[
         # Install marker file in the package index
